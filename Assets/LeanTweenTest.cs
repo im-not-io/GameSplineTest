ce75fb717b6f9aac2e83c8f1848342853f8ef96f
psntnick <perfectalgorithm@gmail.com>
2017-10-08 19:52:39 -0700

    first commit. Test of potential smooth point system using LeanTween Unity add on. needs to be tested with mouse click/ or mouse drag.

---
+++ Assets/LeanTweenTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanTweenTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void SplineMove() {
+		Debug.Log ("spline move triggered");
+		LTSpline ltSpline = new LTSpline( new Vector3[] { new Vector3(0f,0f,0f),new Vector3(0f,0f,0f), new Vector3(3f,3f,0f), new Vector3(10f,0f,0f), new Vector3(0f,0f,0f), new Vector3(0f,0f,0f)} );
+		LeanTween.moveSpline(gameObject, ltSpline, 4.0f).setOrientToPath(true).setDelay(1f); // animate 
+		Vector3 pt = ltSpline.point( 0.6f ); // retrieve a point along the path
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("left click trigger");
+			SplineMove ();
+		}
+	}
+
+
+}
